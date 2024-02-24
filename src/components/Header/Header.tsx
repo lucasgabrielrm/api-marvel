146e6936f5af42778a4b98c7db9e2b108d016aa7
Lucas Gabriel <lucasgmnzs@gmail.com>
2024-02-24 15:35:26 -0300

    Make navbar sticky while scrolling

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,25 +3,25 @@ import './Header.scss';
 
 export const Header = () => {
 
-    let navigate = useNavigate();
 
     return (
-        <header className="header">
-            <a href="/" className="logo">
-                <img src="./images/marvel-logo.png" alt="Marvel Comics logo" />
-            </a>
+        <nav className="navbar">
+            <div className="logo">
+                <a href="/">
+                    <img src="./images/marvel-logo.png" alt="Marvel Comics logo" />
+                </a>
+            </div>
             <div className="links">
                 <ul className="link-items">
                     <li><a href="/">Characters</a></li>
                     <li><a href="/comics">Comics</a></li>
                     <li><a href="/creators">Creators</a></li>
-                    <li><a href="/auth" className="auth-button">Authentication</a></li>
+                    <li><a href="/auth">Authentication</a></li>
                 </ul>
-                
             </div>
             {/* <div className="search-bar">
                 <input type="search" className="search" placeholder="Search Here" />
             </div> */}
-        </header>
+        </nav>
     )
 }
