469420ff3c206b61f30232d3587cf679f110cf9a
Lucas Gabriel <lucasgmnzs@gmail.com>
2024-02-25 11:00:37 -0300

    Set dark mode according to user preference

--- src/components/DarkMode/DarkMode.tsx
+++ src/components/DarkMode/DarkMode.tsx
@@ -1,14 +1,29 @@
+import { useEffect, useState } from "react";
 import { ReactComponent as Sun } from "./Sun.svg";
 import { ReactComponent as Moon } from "./Moon.svg";
+import Cookies from "universal-cookie";
 import "./DarkMode.scss";
 
 const DarkMode = () => {
 
+    const cookies = new Cookies();
+    const [selectedTheme, setSelectedTheme] = useState<string>(cookies.get('colorTheme'));
+
+    useEffect(() => {
+        if (selectedTheme && selectedTheme == 'dark') setDarkMode();
+        else if (selectedTheme && selectedTheme == 'light') setLightMode();
+        else window.matchMedia('(prefers-color-scheme: dark)').matches ? setDarkMode() : setLightMode(); 
+    }, [cookies]);
+
     const setDarkMode = () => {
         document.querySelector('body')?.setAttribute('data-theme', 'dark');
+        cookies.set('colorTheme', 'dark');
+        setSelectedTheme('dark')
     };
     const setLightMode = () => {
         document.querySelector('body')?.setAttribute('data-theme', 'light');
+        cookies.set('colorTheme', 'light');
+        setSelectedTheme('light')
     };
 
     const toggleTheme = (e: any) => {
@@ -23,6 +38,7 @@ const DarkMode = () => {
                 type='checkbox'
                 id='darkmode-toggle'
                 onChange={toggleTheme}
+                defaultChecked={selectedTheme == 'dark'}
             />
             <label className='dark-mode-label' htmlFor='darkmode-toggle'>
                 <Sun />
