585cd45c3f1078d9dd3ca110d818a8d2384d3635
Lucas Gabriel <lucasgmnzs@gmail.com>
2024-02-24 21:10:12 -0300

    Show details in a modal after clicking on an item

--- src/components/Characters/CharactersCard.tsx
+++ src/components/Characters/CharactersCard.tsx
@@ -1,17 +1,27 @@
-import { useNavigate } from "react-router-dom"
+import { Modal } from "@mui/material";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import './CharactersCard.scss';
 
 export const CharactersCard = ({ data }: any) => {
 
     let navigate = useNavigate();
+    const [indexClicked, setIndexClicked] = useState<number>(0);
+    const [modalOpen, setModalOpen] = useState(false);
+
+    const handleOpen = async (index: number) => {
+        setModalOpen(true);
+        setIndexClicked(index);
+    }
 
     return (
         <>
             {
                 (data) ? 
-                    data.map((item: any) => {
+                    data.map((item: any, index: number) => {
                         item.name = item.name.toUpperCase();
                         return (
-                            <div className="card" key={item.id} onClick={() => navigate(`/${item.id}`)}>
+                            <div className="card" key={item.id} onClick={() => handleOpen(index)}>
                                 <img src={`${item.thumbnail.path}.${item.thumbnail.extension}`} alt={`${item.name} face"`} />
                                 <div className="title">
                                     <h3>{item.name}</h3>
@@ -20,6 +30,27 @@ export const CharactersCard = ({ data }: any) => {
                         )
                     }) : ""
             }
+
+            <Modal
+                className="modal-details"
+                open={modalOpen}
+                onClose={() => setModalOpen(false)}
+            >
+                <>
+                    {
+                        (data[indexClicked]) &&
+                        <div className="modal-card" key={data[indexClicked]}>
+                            <div className="modal-image">
+                                <img src={`${data[indexClicked].thumbnail.path}.${data[indexClicked].thumbnail.extension}`} alt={`${data[indexClicked].name} face"`} />
+                            </div>
+                            <div className="modal-text">
+                                <h3>{data[indexClicked].name}</h3>
+                                <p>{data[indexClicked].description}</p>
+                            </div>
+                        </div>
+                    }
+                </>
+            </Modal>
         </>
     )
 }
--- src/components/Creators/CreatorsCard.tsx
+++ src/components/Creators/CreatorsCard.tsx
@@ -1,17 +1,27 @@
-import { useNavigate } from "react-router-dom"
+import { useState } from "react";
+import { Modal } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import './CreatorsCard.scss';
 
 export const CreatorsCard = ({ data }: any) => {
 
     let navigate = useNavigate();
+    const [indexClicked, setIndexClicked] = useState<number>(0);
+    const [modalOpen, setModalOpen] = useState(false);
+
+    const handleOpen = async (index: number) => {
+        setModalOpen(true);
+        setIndexClicked(index);
+    }
 
     return (
         <>
             {
                 (data) ? 
-                    data.map((item: any) => {
+                    data.map((item: any, index: number) => {
                         item.fullName = item.fullName.toUpperCase();
                         return (
-                            <div className="card" key={item.id} onClick={() => navigate(`/${item.id}`)}>
+                            <div className="card" key={item.id} onClick={() => handleOpen(index)}>
                                 <img src={`${item.thumbnail.path}.${item.thumbnail.extension}`} alt={`${item.fullName}"`} />
                                 <div className="title">
                                     <h3>{item.fullName}</h3>
@@ -20,6 +30,27 @@ export const CreatorsCard = ({ data }: any) => {
                         )
                     }) : ""
             }
+
+            <Modal
+                className="modal-details"
+                open={modalOpen}
+                onClose={() => setModalOpen(false)}
+            >
+                <>
+                    {
+                        (data[indexClicked]) &&
+                        <div className="modal-card" key={data[indexClicked]}>
+                            <div className="modal-image">
+                                <img src={`${data[indexClicked].thumbnail.path}.${data[indexClicked].thumbnail.extension}`} alt={`${data[indexClicked].name} face"`} />
+                            </div>
+                            <div className="modal-text">
+                                <h3>{data[indexClicked].fullName}</h3>
+                                <p>Creator in {data[indexClicked].comics.available > 0 ? data[indexClicked].comics.available : 0} comics.</p>
+                            </div>
+                        </div>
+                    }
+                </>
+            </Modal>
         </>
     )
 }
